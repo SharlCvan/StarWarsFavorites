4405f43b19cdf7254e3558cabee26cff2b9591a6
Carl Svahn <calle_tag@hotmail.com>
2020-05-13 12:54:58 +0200

    fixed secured connection to SWAPI

--- funtionality.js
+++ funtionality.js
@@ -54,6 +54,8 @@
         console.log(charracter.homeworld);
 
         const url = `${charracter.homeworld}`;
+        url = url.replace('http', 'https');
+        
         const response = await fetch(url);
         const planetData = await response.json();
 
